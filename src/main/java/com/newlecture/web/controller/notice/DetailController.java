c496c40c491e2d3836f54ea9007c0c0ac4224a74
seeeop2 <seeeop2@gmail.com>
2023-05-20 19:11:21 +0900

    12 Detail 컨트롤러 추가와 View 페이지 집중화의 필요성

--- src/main/java/com/newlecture/web/controller/notice/DetailController.java
+++ src/main/java/com/newlecture/web/controller/notice/DetailController.java
@@ -12,22 +12,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RequestMapping(value = "/notice")
-/*지금 나는 annotation을 이용해서 url mapping을 해주고 있으나,
-* newlecture 강의에서는 dispatcher-servlet(=servlet-context.xml)을 이용해서
-* url mapping 하는 방법을 설명해주고 있다.
-*
-* 현업에서 annotation을 사용할지 xml을 사용할지 랜덤이라 두 가지 방식 모두 숙지하자
-* */
 @Controller
-public class ListController {
+public class DetailController {
+/* ListController에 detail.do 추가해놓은 것을 DetailController에 작성*/
 
     private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
-    @RequestMapping(value = "/list.do", method = RequestMethod.GET)
-    public String List(HttpServletRequest request,
+    @RequestMapping(value = "/detail.do", method = RequestMethod.GET)
+    public String Detail(HttpServletRequest request,
                        HttpServletResponse response,
                        Model model) {
 
-        return "notice/list";
+        return "notice/detail";
     }
 }
